2367b47a4ef76843d3633e281d72c3e67bfaafc8
flybird1971 <flybird1971@gmail.com>
2015-05-27 18:57:48 +0800

    排序算法，调试

---
+++ sort_test.h
@@ -0,0 +1,17 @@
+/*
+* ÅÅĞòËã·¨²âÊÔ£¨²åÈëÅÅĞò£¬Ã°ÅİÅÅĞò£¬¿ìËÙÅÅĞò£¬¶ÑÅÅĞò£¬¹é²¢ÅÅĞò£¬»ùÊıÅÅĞò£©
+*
+* author flybird1971@gmail.com
+* since  2015-05-27
+*/
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include "sort_algorithm.h"
+
+void debugInsertSort();
+void debugQuickSort();
+void debugHeapSort();
+void debugMergeSort();
+void debugradixSort();
